a310b6a4c50df37ccdf73628412fad1fc135bf04
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-11-30 20:33:11 -0500

    wip

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,16 +1,32 @@
 // implement your posts router here
-const express = require('express')
-const Posts = require('./posts-model')
-const router = express.Router()
+const express = require("express");
+const Posts = require("./posts-model");
+const router = express.Router();
 
-router.get('/', (req,res) => {
-    console.log(req.query)
-})
+router.get("/", (req, res) => {
+  Posts.find()
+  .then(found => {
+      res.json(found)
+  })
+  .catch(err => {
+      res.status(500).json({ message: "The posts information could not be retrieved", err: err.message, stack: err.stack})
+  })
+});
 
-router.get('/:id', (req, res) => {})
-router.post('/', (req, res) => {})
-router.put('/:id', (req, res) => {})
-router.delete('/:id', (req, res) => {})
-router.get('/:id/comments', (req, res) => {})
+router.get("/:id", (req, res) => {
+  res.json("foo");
+});
+router.post("/", (req, res) => {
+  res.json("foo");
+});
+router.put("/:id", (req, res) => {
+  res.json("foo");
+});
+router.delete("/:id", (req, res) => {
+  res.json("foo");
+});
+router.get("/:id/comments", (req, res) => {
+  res.json("foo");
+});
 
-module.exports = router
+module.exports = router;
