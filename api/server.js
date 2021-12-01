b78a8d5da94f1616d72bdcdf06c61307d72c4c3f
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-11-30 20:26:35 -0500

    wip

--- api/server.js
+++ api/server.js
@@ -6,6 +6,10 @@ const server = express()
 
 server.use(express.json())
 
+const postsRouter = require('./posts/posts-router')
+server.use('/api/posts', postsRouter)
+
+
 server.use('*', (req, res) => {
     res.status(404).json({
         message: "Your request is not found"
