8d28e45b6315a25988e17dbe4575826483bd7023
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-11-30 22:33:38 -0500

    wip

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -5,26 +5,54 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   Posts.find()
-  .then(found => {
-      res.json(found)
-  })
-  .catch(err => {
-      res.status(500).json({ message: "The posts information could not be retrieved", err: err.message, stack: err.stack})
-  })
+    .then((found) => {
+      res.json(found);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({
+          message: "The posts information could not be retrieved",
+          err: err.message,
+          stack: err.stack,
+        });
+    });
 });
 
 router.get("/:id", (req, res) => {
-  res.json("foo");
+  Posts.findById(req.params.id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist" });
+      }
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({
+          message: "The posts information could not be retrieved",
+          err: err.message,
+          stack: err.stack,
+        });
+    });
 });
+
 router.post("/", (req, res) => {
   res.json("foo");
 });
+
 router.put("/:id", (req, res) => {
   res.json("foo");
 });
+
 router.delete("/:id", (req, res) => {
   res.json("foo");
 });
+
 router.get("/:id/comments", (req, res) => {
   res.json("foo");
 });
